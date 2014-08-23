72488d358f97c76f871979d6230a44b75bf30a40
Elringus <elringus@yandex.ru>
2014-08-23 15:05:00 +0400

    state serialization in a separate thread

--- Assets/_scripts/State/FileState.cs
+++ Assets/_scripts/State/FileState.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Xml.Serialization;
+using System.ComponentModel;
 
 public class FileState : BaseState
 {
 	private const string FILE_NAME = "game-state.xml";
+	private BackgroundWorker asyncWriter;
+	private bool postponedWrite;
+
+	public FileState ()
+		: base()
+	{
+		asyncWriter = new BackgroundWorker();
+		asyncWriter.DoWork += new DoWorkEventHandler(WriteDataAsync);
+		asyncWriter.RunWorkerCompleted +=
+				new RunWorkerCompletedEventHandler(
+					(object sender, RunWorkerCompletedEventArgs e) => { if (postponedWrite) { postponedWrite = false; asyncWriter.RunWorkerAsync(); } }
+				);
+	}
 
 	public static IState Load ()
 	{
@@ -32,10 +46,16 @@ public class FileState : BaseState
 	{
 		if (!base.Save()) return false;
 
+		if (asyncWriter.IsBusy) postponedWrite = true;
+		else asyncWriter.RunWorkerAsync();
+
+		return true;
+	}
+
+	private void WriteDataAsync (object sender, DoWorkEventArgs e)
+	{
 		var serializer = new XmlSerializer(typeof(BaseState), new Type[] { typeof(FileState) });
 		using (var stream = new FileStream(Path.Combine(Environment.CurrentDirectory, FILE_NAME), FileMode.Create))
 			serializer.Serialize(new StreamWriter(stream, System.Text.Encoding.UTF8), this);
-
-		return true;
 	}
 }
