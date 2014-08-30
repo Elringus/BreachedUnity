df59eae2d8a0c1f87fcfd70d831370634a5ebb92
Elringus <elringus@yandex.ru>
2014-08-30 16:40:37 +0400

    base ui logic

--- Assets/_scripts/View/BaseView.cs
+++ Assets/_scripts/View/BaseView.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class BaseView : MonoBehaviour
 {
 	protected static IState State;
 	protected static ILogger Logger;
 	protected static IText Text;
+
+	private Transform uiCanvas;
 
 	static BaseView ()
 	{
@@ -38,7 +41,9 @@ public abstract class BaseView : MonoBehaviour
 
 	protected virtual void Start ()
 	{
-
+		var versionText = AddUiElement("text_version").GetComponent<Text>();
+		versionText.text = string.Format("Breached {0}\nver. {1}.{2}.{3}", 
+			GlobalConfig.RELEASE_TYPE, GlobalConfig.VERSION_MAJOR, GlobalConfig.VERSION_MIDDLE, GlobalConfig.VERSION_MINOR);
 	}
 
 	protected virtual void Update ()
@@ -49,5 +54,15 @@ public abstract class BaseView : MonoBehaviour
 	protected void SwitchView (ViewType to)
 	{
 		Application.LoadLevel("scn_" + to);
+	}
+
+	protected GameObject AddUiElement (string prefabName)
+	{
+		if (!uiCanvas) uiCanvas = GameObject.Find("_ui").transform;
+
+		GameObject uiElement = Instantiate(Resources.Load(prefabName)) as GameObject;
+		uiElement.transform.parent = uiCanvas;
+
+		return uiElement;
 	}
 }
