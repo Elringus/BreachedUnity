2ec6616855e50dda881f76f93298efba37b580b3
Elringus <elringus@yandex.ru>
2014-10-04 13:34:07 +0400

    optimize simple view layout

--- Assets/_scripts/View/SimpleView.cs
+++ Assets/_scripts/View/SimpleView.cs
@@ -64,24 +64,25 @@ public class SimpleView : BaseView
 		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(width), GUILayout.Height(Screen.height - 35));
 
 		GUILayout.BeginHorizontal();
-		GUILayout.Label(string.Format("Day: {0}/{1}", State.CurrentDay, State.TotalDays));
-		GUILayout.Label(string.Format("AP: {0}/{1}", State.CurrentAP, State.MaxAP));
+		if (!inFlightMode && QuestController.GetCurrentQuest() == null)
+		{
+			if (GUILayout.Button(selectedPage == SimpleViewPage.Bridge ? "<b>❖ Bridge</b>" : "Bridge")) selectedPage = SimpleViewPage.Bridge;
+			if (GUILayout.Button(selectedPage == SimpleViewPage.Workshop ? "<b>❖ Workshop</b>" : "Workshop")) selectedPage = SimpleViewPage.Workshop;
+			if (GUILayout.Button(selectedPage == SimpleViewPage.Map ? "<b>❖ Map</b>" : "Map")) selectedPage = SimpleViewPage.Map;
+			if (GUILayout.Button(selectedPage == SimpleViewPage.Horizon ? "<b>❖ Horizon</b>" : "Horizon")) selectedPage = SimpleViewPage.Horizon;
+		}
 		GUILayout.EndHorizontal();
 
-		GUILayout.BeginHorizontal();
-		if (GUILayout.Button("Bridge")) selectedPage = SimpleViewPage.Bridge;
-		if (GUILayout.Button("Workshop")) selectedPage = SimpleViewPage.Workshop;
-		if (GUILayout.Button("Map")) selectedPage = SimpleViewPage.Map;
-		if (GUILayout.Button("Horizon")) selectedPage = SimpleViewPage.Horizon;
-		GUILayout.EndHorizontal();
+		GUILayout.Box(string.Format("Day: {0}/{1} {2} AP: {3}/{4}",
+			State.CurrentDay, State.TotalDays, QuestController.GetCurrentQuest() != null ? string.Format("● In quest mode. Quest name: {0}. Quest progress: {1} ●",
+					QuestController.GetCurrentQuest().ID, QuestController.GetCurrentQuest().CurrentBlock) :
+			inFlightMode ? string.Format("● In flight mode. Minerals: A{0} B{1} C{2}. Loot charges: {3}/{4} ●", State.MineralA, State.MineralB, State.MineralC, lootCharges, State.LootCharges) : 
+			"●", State.CurrentAP, State.MaxAP));
 
 		if (State.GameStatus == GameStatus.InProgress)
 		{
 			if (inFlightMode)
 			{
-				GUILayout.Box(string.Format("In flight mode. Minerals: A{0} B{1} C{2}. Loot charges: {3}/{4}", 
-					State.MineralA, State.MineralB, State.MineralC, lootCharges, State.LootCharges));
-
 				for (int i = 0; i < sectorLoot.Count; i++ )
 					if (GUILayout.Button(string.Format("Loot spot of {0}", sectorLoot[i].LootType)))
 					{
@@ -95,8 +96,6 @@ public class SimpleView : BaseView
 			}
 			else if (QuestController.GetCurrentQuest() != null)
 			{
-				GUILayout.Box(string.Format("In quest mode. Quest name: {0}. Quest progress: {1}",
-					QuestController.GetCurrentQuest().ID, QuestController.GetCurrentQuest().CurrentBlock), GUILayout.Width(width - 10));
 				GUILayout.Label(XDocument.Parse(Text.Get(QuestController.GetCurrentQuest().CurrentBlock)).Root.Value, GUILayout.Width(width - 10));
 				var choises = XDocument.Parse(Text.Get(QuestController.GetCurrentQuest().CurrentBlock)).Root.Elements("choise");
 				if (choises.Count() == 0) { if (GUILayout.Button("End quest")) QuestController.EndQuest(); }
@@ -106,8 +105,6 @@ public class SimpleView : BaseView
 			{
 				if (selectedPage == SimpleViewPage.Bridge)
 				{
-					GUILayout.Box("Bridge");
-
 					for (int i = 1; i <= State.CurrentDay; i++)
 						foreach (var record in State.JournalRecords.Where(r => r.AssignedDay == i))
 							GUILayout.Label(Text.Get(record.ID));
@@ -117,11 +114,17 @@ public class SimpleView : BaseView
 
 				if (selectedPage == SimpleViewPage.Workshop)
 				{
-					GUILayout.Box("Workshop");
-
 					GUILayout.BeginHorizontal();
 					GUILayout.Label(string.Format("Fuel tank: <b>{0}</b>", State.FuelSynthed ? "FULL" : "EMPTY"));
 					GUILayout.Label(string.Format("Breakage: <b>{0}</b>", State.EngineFixed ? "FIXED" : State.BreakageType.ToString()));
+					GUILayout.EndHorizontal();
+
+					GUILayout.BeginHorizontal();
+					GUILayout.Label(string.Format("Resources: W{0} A{1} C{2}", State.Wiring, State.Alloy, State.Chips), GUILayout.Width(width / 2));
+					GUILayout.Label(string.Format("Fix engine requirments: W{0} A{1} C{2}",
+						State.FixEngineRequirements[State.BreakageType][0],
+						State.FixEngineRequirements[State.BreakageType][1],
+						State.FixEngineRequirements[State.BreakageType][2]));
 					GUILayout.EndHorizontal();
 
 					GUILayout.BeginHorizontal();
@@ -137,13 +140,14 @@ public class SimpleView : BaseView
 					GUILayout.EndHorizontal();
 
 					GUILayout.BeginHorizontal();
-					GUILayout.Label(string.Format("Resources: W{0} A{1} C{2}", State.Wiring, State.Alloy, State.Chips), GUILayout.Width(width / 2));
-					GUILayout.Label(string.Format("Fix engine requirments: W{0} A{1} C{2}",
-						State.FixEngineRequirements[State.BreakageType][0],
-						State.FixEngineRequirements[State.BreakageType][1],
-						State.FixEngineRequirements[State.BreakageType][2]));
-					GUILayout.EndHorizontal();
+					GUILayout.BeginVertical(GUILayout.Width(width / 2));
+					GUILayout.Label(string.Format("Analyzed artifacts ({0}):", State.Artifacts.FindAll(x => x.Status == ArtifactStatus.Analyzed).Count));
+					showArtifacts = GUILayout.Toggle(showArtifacts, "show all analyzed artifacts");
+					if (showArtifacts) foreach (var artifact in State.Artifacts.FindAll(x => x.Status == ArtifactStatus.Analyzed))
+							GUILayout.Label(string.Format("[{0}] {1} ScanInfo: {2}", artifact.ID, artifact.Name, artifact.ScanInfo));
+					GUILayout.EndVertical();
 
+					GUILayout.BeginVertical(GUILayout.Width(width / 2 - 30));
 					GUILayout.Label(string.Format("Fuel synth probes ({0}):", State.FuelSynthProbes.Count));
 					showProbes = GUILayout.Toggle(showProbes, "show all the probes");
 					if (showProbes) foreach (var probe in State.FuelSynthProbes)
@@ -151,14 +155,11 @@ public class SimpleView : BaseView
 							GUILayout.BeginHorizontal();
 							GUILayout.Label(string.Format("Probe №{0} [A: {1}, B: {2}, C: {3}] is {4}.",
 								State.FuelSynthProbes.FindIndex(x => x == probe), probe[0], probe[1], probe[2],
-								workshopController.MeasureProbe(probe)), GUILayout.Width(width / 2));
+								workshopController.MeasureProbe(probe)));
 							GUILayout.EndHorizontal();
 						}
-
-					GUILayout.Label(string.Format("Analyzed artifacts ({0}):", State.Artifacts.FindAll(x => x.Status == ArtifactStatus.Analyzed).Count));
-					showArtifacts = GUILayout.Toggle(showArtifacts, "show all analyzed artifacts");
-					if (showArtifacts) foreach (var artifact in State.Artifacts.FindAll(x => x.Status == ArtifactStatus.Analyzed))
-						GUILayout.Label(string.Format("[{0}] {1} ScanInfo: {2}", artifact.ID, artifact.Name, artifact.ScanInfo), GUILayout.Width(width - 10));
+					GUILayout.EndVertical();
+					GUILayout.EndHorizontal();
 
 					if (workshopController.CanFixEngine() &&
 						GUILayout.Button(string.Format("Fix engine [-{0}AP]", State.FixEngineAPCost))) workshopController.FixEngine();
@@ -182,7 +183,6 @@ public class SimpleView : BaseView
 
 				if (selectedPage == SimpleViewPage.Map)
 				{
-					GUILayout.Box("Map");
 					if (GUILayout.Button(string.Format("Enter 1st sector [-{0}AP]", State.EnterSectorAPCost))) InitFlightMode(1);
 					if (GUILayout.Button(string.Format("Enter 2nt sector [-{0}AP]", State.EnterSectorAPCost))) InitFlightMode(2);
 					if (GUILayout.Button(string.Format("Enter 3rd sector [-{0}AP]", State.EnterSectorAPCost))) InitFlightMode(3);
@@ -191,7 +191,6 @@ public class SimpleView : BaseView
 
 				if (selectedPage == SimpleViewPage.Horizon)
 				{
-					GUILayout.Box("Horizon");
 					foreach (var phrase in horizonController.GetPhrases())
 					{
 						string phraseText = string.Format("[{0}] {1}", phrase.ID, Text.Get(phrase.ID));
