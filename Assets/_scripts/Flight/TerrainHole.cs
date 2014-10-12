ce45be469cd29a40d29959b843598a93d74bb345
Elringus <elringus@yandex.ru>
2014-10-12 17:42:26 +0400

    add terrain holes handler

---
+++ Assets/_scripts/Flight/TerrainHole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainHole : MonoBehaviour
+{
+	private void OnTriggerEnter (Collider colli)
+	{
+		if (colli.CompareTag("Player")) 
+			Physics.IgnoreLayerCollision(8, 9, true); 
+	}
+
+	private void OnTriggerExit (Collider colli)
+	{
+		if (colli.CompareTag("Player"))
+			Physics.IgnoreLayerCollision(8, 9, false); 
+	}
+}
