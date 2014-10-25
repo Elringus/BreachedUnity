f612f7863ecf357d499cca09d93955fb468da75f
Elringus <elringus@yandex.ru>
2014-10-25 14:32:01 +0400

    deleting directory

--- Assets/Plugins/x86/depends/CheckDependencies.bat
+++
@@ -1,5 +0,0 @@
-set thisdir=%CD%
-set PATH=%thisdir%\..\dependencies;%PATH%
-
-depends.exe /c /f1 /ot:RenderDependencies.log ..\TrueSkyPluginRender_MT.dll
-depends.exe /c /f1 /ot:UIDependencies.log ..\TrueSkyUI_MD.dll
