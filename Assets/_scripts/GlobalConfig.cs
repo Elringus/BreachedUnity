bd454e733d9b07cfb1cbb360b29bc31fc6a16f93
Elringus <elringus@yandex.ru>
2015-01-25 19:58:15 +0300

    update version

--- Assets/_scripts/GlobalConfig.cs
+++ Assets/_scripts/GlobalConfig.cs
@@ -5,5 +5,5 @@ public static class GlobalConfig
 
 	public static readonly int VERSION_MAJOR =    0;
 	public static readonly int VERSION_MIDDLE =   3;
-	public static readonly int VERSION_MINOR =    20150122;
+	public static readonly int VERSION_MINOR =    20150125;
 }
