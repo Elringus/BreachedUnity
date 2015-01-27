4a939da2914e09e91bb6985801904751805575ef
Elringus <elringus@yandex.ru>
2015-01-27 14:53:18 +0300

    refactor logic, unsync tables downloading

--- Assets/_scripts/Text/GoogleText.cs
+++ Assets/_scripts/Text/GoogleText.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
-using System.Linq;
 using System.Net;
 using System.IO;
 using System;
@@ -16,87 +15,59 @@ public class GoogleText : IText
 	private const string PHRASES_TABLE_URL = @"https://docs.google.com/spreadsheets/d/1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk/export?format=csv&id=1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk&gid=1387093746";
 	private const string ARTIFACTS_TABLE_URL = @"https://docs.google.com/spreadsheets/d/1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk/export?format=csv&id=1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk&gid=222661492";
 	private const string SCANINFO_TABLE_URL = @"https://docs.google.com/spreadsheets/d/1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk/export?format=csv&id=1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk&gid=1514799521";
+	private const string TOOLTIPS_TABLE_URL = @"https://docs.google.com/spreadsheets/d/1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk/export?format=csv&id=1DztvoxCDKT2abEwqtxuRj2oQZNbtPAnYOwrj7tHvphk&gid=469296090";
 
 	private static float downloadProgress;
 
-	private Dictionary<string, string> cachedText = new Dictionary<string, string>();
+	private Dictionary<string, string> cachedText;
 
 	public GoogleText ()
 	{
-		Thread thread = new Thread(RetrieveData);
-		thread.Start();
-	}
-
-	private void RetrieveData ()
-	{
+		cachedText = new Dictionary<string, string>();
 		downloadProgress = 0;
 
+		new Thread(() => { RetrieveTable(QUESTS_TABLE_URL); }).Start();
+		new Thread(() => { RetrieveTable(JOURNAL_TABLE_URL); }).Start();
+		new Thread(() => { RetrieveTable(PHRASES_TABLE_URL); }).Start();
+		new Thread(() => { RetrieveTable(ARTIFACTS_TABLE_URL); }).Start();
+		new Thread(() => { RetrieveTable(SCANINFO_TABLE_URL); }).Start();
+		new Thread(() => { RetrieveTable(TOOLTIPS_TABLE_URL); }).Start();
+	}
+
+	private void RetrieveTable (string tableURL)
+	{
 		try
 		{
 			SSLValidator.OverrideValidation();
 
-			var request = (HttpWebRequest)WebRequest.Create(QUESTS_TABLE_URL);
+			var request = (HttpWebRequest)WebRequest.Create(tableURL);
 			request.Method = WebRequestMethods.Http.Get;
 			request.Timeout = REQUEST_TIMEOUT;
 			var response = (HttpWebResponse)request.GetResponse();
 			var reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-			List<string> rows = new List<string>(reader.ReadToEnd().Split((char)10));
+			var rows = new List<string>(reader.ReadToEnd().Split((char)10));
 
-			downloadProgress = .2f;
-
-			request = (HttpWebRequest)WebRequest.Create(JOURNAL_TABLE_URL);
-			response = (HttpWebResponse)request.GetResponse();
-			reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-			rows.AddRange(reader.ReadToEnd().Split((char)10));
-
-			downloadProgress = .4f;
-
-			request = (HttpWebRequest)WebRequest.Create(PHRASES_TABLE_URL);
-			response = (HttpWebResponse)request.GetResponse();
-			reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-			rows.AddRange(reader.ReadToEnd().Split((char)10));
-
-			downloadProgress = .6f;
-
-			request = (HttpWebRequest)WebRequest.Create(ARTIFACTS_TABLE_URL);
-			response = (HttpWebResponse)request.GetResponse();
-			reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-			rows.AddRange(reader.ReadToEnd().Split((char)10));
-
-			downloadProgress = .8f;
-
-			request = (HttpWebRequest)WebRequest.Create(SCANINFO_TABLE_URL);
-			response = (HttpWebResponse)request.GetResponse();
-			reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-			rows.AddRange(reader.ReadToEnd().Split((char)10));
-
-			downloadProgress = 1;
-
-			var result = new Dictionary<string, string>();
 			foreach (var row in rows)
 			{
 				string key = row.Split(',')[0];
-				if (result.ContainsKey(key)) continue;
+				if (cachedText.ContainsKey(key)) continue;
 				string value = row
 					.Replace(key + ',', string.Empty)
 					.Replace("\"", string.Empty)
 					.Replace("<br>", System.Environment.NewLine);
-				result.Add(key, value);
+				cachedText.Add(key, value);
 			}
-
-			cachedText = result;
 		}
 		catch (Exception e)
 		{
 			ServiceLocator.Logger.Log(e.Message);
-			Thread.Sleep(1000);
-			RetrieveData();
+			Thread.Sleep(500);
+			RetrieveTable(tableURL);
 			return;
 		}
 
-		Events.RaiseTextUpdated();
-
-		Thread.CurrentThread.Abort();
+		downloadProgress += .17f;
+		if (downloadProgress > 1) Events.RaiseTextUpdated();
 	}
 
 	public string Get (string term)
