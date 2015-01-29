b0f32f3bad21aaba2b7c8af75f94f060997d3bfd
Elringus <elringus@yandex.ru>
2015-01-29 19:29:52 +0300

    1st sector

--- Assets/_scripts/GlobalConfig.cs
+++ Assets/_scripts/GlobalConfig.cs
@@ -5,5 +5,5 @@ public static class GlobalConfig
 
 	public static readonly int VERSION_MAJOR =    0;
 	public static readonly int VERSION_MIDDLE =   3;
-	public static readonly int VERSION_MINOR =    20150128;
+	public static readonly int VERSION_MINOR =    20150129;
 }
