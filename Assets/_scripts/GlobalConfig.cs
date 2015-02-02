fc72602fba9727a0658e0ee7afd492ae8fd73db6
Elringus <elringus@yandex.ru>
2015-02-02 19:48:24 +0300

    update version

--- Assets/_scripts/GlobalConfig.cs
+++ Assets/_scripts/GlobalConfig.cs
@@ -1,9 +1,9 @@
 ﻿
 public static class GlobalConfig
 {
-	public static readonly ReleaseType RELEASE_TYPE = ReleaseType.beta;
+	public static readonly ReleaseType RELEASE_TYPE = ReleaseType.alpha;
 
 	public static readonly int VERSION_MAJOR =    0;
 	public static readonly int VERSION_MIDDLE =   4;
-	public static readonly int VERSION_MINOR =    20150201;
+	public static readonly int VERSION_MINOR =    20150202;
 }
