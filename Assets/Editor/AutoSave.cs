135dd9aeedf73f355db30ccc308a2b35d0088fe0
Elringus <elringus@yandex.ru>
2015-02-05 17:50:26 +0300

    remove unused imports

--- Assets/Editor/AutoSave.cs
+++ Assets/Editor/AutoSave.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
 
 [InitializeOnLoad]
 public class AutoSave
